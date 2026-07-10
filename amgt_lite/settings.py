db565baa672604c0dac37bef7363080e4c94bc9b
Juan David Guzmán Gallego <117551190+juandajedrez@users.noreply.github.com>
2026-07-10 18:31:29 -0500

    hosts

--- amgt_lite/settings.py
+++ amgt_lite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6-8y18nyya2lz=z7itr9y2hbsgcc-gdyo$^qcru795(_)xp%i+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
